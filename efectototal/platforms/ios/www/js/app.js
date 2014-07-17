fca6843228283accf6543331c5140d26a214dbe1
aresavazquez <ares _vb>
2014-07-16 22:03:43 -0500

    pantallas

--- efectototal/platforms/ios/www/js/app.js
+++ efectototal/platforms/ios/www/js/app.js
@@ -81,6 +81,15 @@ angular.module('efectototal', ['ionic', 'openfb', 'efectototal.controllers'])
       }
     })
 
+        .state('app.perfil_informacion', {
+      url: "/perfil_actividad",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/profile_informacion.html"
+        }
+      }
+    })
+
     .state('app.videos', {
       url: "/videos",
       views: {
